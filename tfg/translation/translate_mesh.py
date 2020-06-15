4003cec3cddd9aaa59b8e161e63c1ad086259c1b
Martín Quesada <martnquesada@gmail.com>
2020-06-15 16:53:46 +0200

    added kenlm as requirement, loading kenlm models to translate mesh is now possible

--- tfg/translation/translate_mesh.py
+++ tfg/translation/translate_mesh.py
@@ -5,6 +5,7 @@ from tfg.utilities import parse_mesh
 import pickle
 from tqdm import tqdm
 from tfg.translation.vecmap_ngram_translator import VecmapNgramTranslator
+from kenlm import LanguageModel
 
 
 def main():
@@ -13,7 +14,8 @@ def main():
     parser.add_argument('--translation_phrase_table', help='Translation phrase-table')
     parser.add_argument('--phrase_table', default=True, action='store_true',
                         help='True if the translation file is in phrase-table format')
-    parser.add_argument('--target_language_model', help='Pickle file containing target language model')
+    parser.add_argument('--mle_lm', help='Pickle file containing target language model')
+    parser.add_argument('--kenlm_lm', help='Binary file containing target language model')
     parser.add_argument('--translated_mesh', help='Translated Mesh thesaurus')
     args = parser.parse_args()
 
@@ -26,8 +28,15 @@ def main():
         translation_dict = phrase_table_to_dict(args.translation_phrase_table)
     else:
         translation_dict = validation_file_to_dict(args.translation_phrase_table)
-    print("Loading language model")
-    target_language_model = pickle.load(open(args.target_language_model, 'rb'))
+    if args.mle_lm:
+        print("Loading NLTK MLE language model")
+        target_language_model = pickle.load(open(args.mle_lm, 'rb'))
+    elif args.kenlm_lm:
+        print("Loading kenlm language model")
+        target_language_model = LanguageModel(args.kenlm_lm)
+    else:
+        print('No language model given!')
+        return 1
     translator = VecmapNgramTranslator(translation_dict, target_language_model)
 
     # translating parsed mesh thesaurus
@@ -35,8 +44,13 @@ def main():
     for descriptor in tqdm(descriptor_list):
         for concept in descriptor['concepts']:
             for term in concept['terms']:
-                translated_term = ' '.join(translator.ngram_translation(
-                    term['name'].lower().split(), topk=3, use_lm=True, lm_scaling=0.1, lex_scaling=1.0, beam_size=10))
+                if args.mle_lm:
+                    translated_term = ' '.join(translator.mle_ngram_translation(
+                        term['name'].lower().split(), topk=3, use_lm=True, lm_scaling=0.1, lex_scaling=1.0, beam_size=10))
+                else:
+                    translated_term = ' '.join(translator.kenlm_ngram_translation(
+                        term['name'].lower().split(), topk=3, use_lm=True, lm_scaling=0.1, lex_scaling=1.0,
+                        beam_size=10))
                 term['name'] = translated_term
 
     # storing parsed translated thesaurus as a serialized python object
